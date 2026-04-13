1b74316ac5f4a5ee09f5df3b03afeded3539b2eb
Arti <34264959+kumariartidev@users.noreply.github.com>
2026-04-13 08:02:09 +0000

    added varibales test

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 123456;
+let accountEmail = "arti@gmail.com";
+var accountPassword = "1234";
+accountCity = "Delhi";
+let accounState;
+
+// accountCity = 789012; // This will throw an error because accountID is a constant
+// console.log(accountID);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+//console.table([accountID, accountEmail, accountPassword, accountCity, accounState]);
+console.log([typeof undefined]);
